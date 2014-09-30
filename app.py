997ac6efe8d753ebc230bb09eeb2c450e2d48644
Alberto Burgos <albertoburgosmh@gmail.com>
2014-09-30 23:08:53 +0100

    Create /api/1/reload to update JSON files

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 import config
+import os
+import subprocess
 from lib.bottle import route, run, static_file
+
+curr_dir = os.path.dirname(os.path.realpath(__file__))
 
 @route("/api/1/repo/<info>")
 def repo(info):
@@ -14,5 +18,9 @@ def heads(remote):
 @route("/api/1/logs/<remote>")
 def logs(remote):
     return static_file(remote + ".json", root=config.logs_dir)
+
+@route("/api/1/reload")
+def reload():
+    return subprocess.call([curr_dir + "/shell/generate-data", curr_dir + "/config.py"])
 
 run(host="localhost", port=8123)
