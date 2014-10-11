0702477c809463d93ad03ac786d06ca8b1b5bb1c
Alberto Burgos <albertoburgosmh@gmail.com>
2014-10-11 11:01:11 +0100

    Create API to add information to a repository

--- src/python/app.py
+++ src/python/app.py
@@ -2,9 +2,10 @@
 
 import bottle
 import config
+import json
 import os
 import subprocess
-from bottle import response, route, static_file
+from bottle import post, request, response, route, static_file
 
 curr_dir = os.path.dirname(os.path.realpath(__file__))
 shell_dir = curr_dir + "/../shell"
@@ -17,6 +18,17 @@ def repos():
 @route("/api/1/repos/<repo>/commits")
 def logs(repo):
     return static_file("commits_" + repo + ".json", root=config.data_dir, mimetype="application/json")
+
+@route("/api/1/repos/<repo>/info")
+def logs(repo):
+    return static_file("info_" + repo + ".json", root=config.data_dir, mimetype="application/json")
+
+@post("/api/1/repos/<repo>/info")
+def logs(repo):
+    response.content_type = "application/json; charset=UTF8"
+    with open(config.data_dir + "/info_" + repo + ".json", "w") as f:
+        f.write(json.dumps(request.json))
+    return '{ "result": "0" }';
 
 @route("/api/1/reload")
 def reload():
